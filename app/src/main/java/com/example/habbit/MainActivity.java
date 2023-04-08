ff29a7a76834f375bcfdf8a1508a763e07130d7f
Esmaeil <smaelll7554@gmail.com>
2023-04-08 17:51:01 +0330

    toolbar added included add filter and menu

--- app/src/main/java/com/example/habbit/MainActivity.java
+++ app/src/main/java/com/example/habbit/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.habbit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton m_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        m_add = findViewById(R.id.add);
+        m_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddRoutine(v);
+            }
+        });
+    }
+    public void openAddRoutine(View view){
+        Intent intent = new Intent(this, addRoutine.class);
+        startActivity(intent);
     }
 }
